4837e1892b338224e0891119bf29f67e7ce7ed4e
CocoaGabby <softwaredevsooc@gmail.com>
2023-03-14 00:13:05 -0400

    Implemented Armor

--- src/main/java/com/elitemastereric/obsidianarmor/items/ModItems.java
+++ src/main/java/com/elitemastereric/obsidianarmor/items/ModItems.java
@@ -1,11 +1,14 @@
 package com.elitemastereric.obsidianarmor.items;
 
 import com.elitemastereric.obsidianarmor.ObsidianArmorMod;
+import com.elitemastereric.obsidianarmor.materials.ObsidianArmorMaterial;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -15,6 +18,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.flag.FeatureFlagSet;
 
 public class ModItems {
@@ -23,13 +27,22 @@ public class ModItems {
     // The deferred registry.
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ObsidianArmorMod.MOD_ID);
 
-    // Armor
-    // public static final Item OBSIDIAN_CHESTPLATE = new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant());
-    public static final RegistryObject<Item> OBSIDIAN_CHESTPLATE = ITEMS.register("obsidian_chestplate", () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+    // Materials
+    public static final ArmorMaterial OBSIDIAN_ARMOR_MATERIAL = new ObsidianArmorMaterial();
+    
+
+    // Armor Items
+    public static final RegistryObject<Item> OBSIDIAN_HELMET = 
+        ITEMS.register("obsidian_helmet", () -> new ArmorItem(OBSIDIAN_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+    public static final RegistryObject<Item> OBSIDIAN_CHESTPLATE = 
+        ITEMS.register("obsidian_chestplate", () -> new ArmorItem(OBSIDIAN_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+    public static final RegistryObject<Item> OBSIDIAN_LEGGINGS = 
+        ITEMS.register("obsidian_leggings", () -> new ArmorItem(OBSIDIAN_ARMOR_MATERIAL, EquipmentSlot.LEGS, new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+    public static final RegistryObject<Item> OBSIDIAN_BOOTS = 
+        ITEMS.register("obsidian_boots", () -> new ArmorItem(OBSIDIAN_ARMOR_MATERIAL, EquipmentSlot.FEET, new Item.Properties().rarity(Rarity.RARE).fireResistant()));
 
     // Creative Tab
     public static CreativeModeTab ITEM_GROUP;
-
     public static final ResourceLocation ITEM_GROUP_LOCATION = new ResourceLocation(ObsidianArmorMod.MOD_ID, "obsidian_armor");
 
     public static void onInitialize(IEventBus eventBus) {
@@ -51,6 +64,9 @@ public class ModItems {
 
     private static void addCreativeModeTabItems(FeatureFlagSet featureFlagSet, CreativeModeTab.Output output, boolean bl) {
         // Add items to the creative tab.
+        output.accept(OBSIDIAN_HELMET.get());
         output.accept(OBSIDIAN_CHESTPLATE.get());
+        output.accept(OBSIDIAN_LEGGINGS.get());
+        output.accept(OBSIDIAN_BOOTS.get());
     }
 }
---
+++ src/main/java/com/elitemastereric/obsidianarmor/materials/ObsidianArmorMaterial.java
@@ -0,0 +1,61 @@
+package com.elitemastereric.obsidianarmor.materials;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Items;
+
+public class ObsidianArmorMaterial implements ArmorMaterial {
+
+    private static final String NAME = "obsidian";
+    private static final float TOUGHNESS = 0.0F;
+    private static final int[] DURABILITY = new int[] {137, 158, 168, 116}; // Durability of Boots, Leggings, Chestplate, Helmet
+    private static final int[] PROTECTION = new int[] {2, 4, 6, 2}; // Armor points of Boots, Leggings, Chestplate, Helmet
+    private static final int ENCHANTABILITY = 20;
+    private static final SoundEvent EQUIP_SOUND = SoundEvents.ARMOR_EQUIP_DIAMOND;
+    private static final Ingredient REPAIR_INGREDIENT = Ingredient.of(Items.OBSIDIAN);
+    private static final float KNOCKBACK_RESISTANCE = 0.0f;
+
+    @Override
+    public int getDurabilityForSlot(EquipmentSlot slot) {
+        return DURABILITY[slot.getIndex()];
+    }
+
+    @Override
+    public int getDefenseForSlot(EquipmentSlot slot) {
+        return PROTECTION[slot.getIndex()];
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return ENCHANTABILITY;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return EQUIP_SOUND;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return REPAIR_INGREDIENT;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public float getToughness() {
+        return TOUGHNESS;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return KNOCKBACK_RESISTANCE;
+    }
+    
+}
